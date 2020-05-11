aa0f7508c26f3fb2351a39c7c5c889c10fcb16c3
Philippe PERECHODOV <philippe.perechodov@free.fr>
2020-05-11 08:26:53 +0200

    Réorganisation

--- header.php
+++ header.php
@@ -1,20 +1,19 @@
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
+<html lang="en" dir="ltr"><!-- "ltr" veut dire "Left To Right", c'est le sens de lecture -->
 
 <head>
   <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"><!-- "viewport" est utile pour adapter la page aux différents formats d'écran -->
   <meta name="author" content="Philippe PERECHODOV">
   <title>Présentation HTML & CSS</title>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"><!-- TOUJOURS mettre le link bootstrap avant le main.css car s'il y a des similitude alors c'est le denrier qui s'applique donc ce sera main.css (ce qu'on a fait) -->
   <link rel="stylesheet" href="main.css">
 </head>
 
 <body>
   <!-- Code de "navbar" pris sur bootstrap -->
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" target="_blank" href="https://www.accesscodeschool.fr/"><img src="img/logo-acs-black.png" alt="logo access code school" id="logo-acs"></a>
-    <!-- le "alt" est à remplir clairement car c'est ce qui sera lu par les bots ou par les logiciels pour malvoyant par exemple -->
+  <nav class="navbar navbar-expand-lg navbar-light bg-light"><!-- on utilise "class" et pas "style" ! -->
+    <a class="navbar-brand" target="_blank" href="https://www.accesscodeschool.fr/"><img src="img/logo-acs-black.png" alt="logo access code school" id="logo-acs"></a><!-- le "alt" est à remplir clairement car c'est ce qui sera lu par les bots ou par les logiciels pour malvoyant par exemple -->
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
--- index.php
+++ index.php
@@ -1,14 +1,12 @@
 <?php
-
 include "header.php";
-
 ?>
 
 <main>
 <div class="container">
   <!-- Code de "jumbotron" pris sur bootstrap -->
   <div class="jumbotron">
-    <h1 class="display-4">Bonjour à toutes et à tous !</h1>
+    <h1 class="display-5">Bonjour à toutes et à tous !</h1>
     <p class="lead">Sur ce site, vous retrouverez toutes les informations utiles pour coder une page web.</p>
     <hr class="my-4">
     <p>Il s'améliorera et se complètera au fur et à mesure de ma formation.<br>
@@ -19,7 +17,7 @@ include "header.php";
   <!-- Code de "card" pris sur bootstrap -->
   <div class="row">
     <!-- Card n°1 -->
-    <div class="col-sm d-flex justify-content-around">
+    <div class="col-sm mb-5 d-flex justify-content-around">
       <div class="card" style="width: 15rem;">
         <img src="img/HTML5.png" class="card-img-top card-img-height" alt="logo HTML5">
         <div class="card-body">
@@ -30,7 +28,7 @@ include "header.php";
       </div>
     </div>
     <!-- Card n°2 -->
-    <div class="col-sm d-flex justify-content-around">
+    <div class="col-sm mb-5 d-flex justify-content-around">
       <div class="card" style="width: 15rem;">
         <img src="img/CSS3.png" class="card-img-top card-img-height" alt="logo CSS3">
         <div class="card-body">
@@ -41,42 +39,61 @@ include "header.php";
       </div>
     </div>
     <!-- Card n°3 -->
-    <div class="col-sm d-flex justify-content-around">
+    <div class="col-sm mb-5 d-flex justify-content-around">
       <div class="card" style="width: 15rem;">
         <img src="img/console.png" class="card-img-top card-img-height" alt="image console">
         <div class="card-body">
           <h5 class="card-title">Command Line</h5>
-          <p class="card-text">Quelle console ? PS4 ou XBOX ? ...ah, non rien à voir...</p>
+          <p class="card-text">Quelle console ? PS4 ou XBOX ? ...ah, non, rien à voir...</p>
           <a href="console.php" class="btn btn-primary">Suivre le lapin blanc</a>
         </div>
       </div>
     </div>
     <!-- Card n°4 -->
-    <div class="col-sm d-flex justify-content-around">
+    <div class="col-sm mb-5 d-flex justify-content-around">
       <div class="card" style="width: 15rem;">
         <img src="img/GitGitHub.jpeg" class="card-img-top card-img-height" alt="logo Git&GitHub">
         <div class="card-body">
           <h5 class="card-title">Git & GitHub</h5>
-          <p class="card-text">Mon nouveau mailleur ami !</p>
+          <p class="card-text">Mon nouveau meilleur ami !</p>
           <a href="git.php" class="btn btn-primary">Go Go Git !</a>
         </div>
       </div>
     </div>
     <!-- Card n°5 -->
-    <div class="col-sm d-flex justify-content-around">
+    <div class="col-sm mb-5 d-flex justify-content-around">
       <div class="card" style="width: 15rem;">
         <img src="img/Atom.webp" class="card-img-top card-img-height" alt="logo Atom">
         <div class="card-body">
           <h5 class="card-title">Atom</h5>
-          <p class="card-text">C'est une bombe !!! </p>
+          <p class="card-text">C'est une bombe ! </p>
           <a href="atom.php" class="btn btn-primary">Boum !</a>
         </div>
       </div>
     </div>
   </div>
+  <div class="row">
+    <div class="col-sm">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+      irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </div>
+    <div class="col-sm">
+      <div class="card" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">C'est Super !</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">On y va !</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+      irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </div>
+  </div>
 </div>
 </main>
 
-  <?php
-
-  include "footer.php";
+<?php
+include "footer.php";
