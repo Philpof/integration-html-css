e8939fdf1841f417da263ed86e7e599b28f37a20
Philippe PERECHODOV <philippe.perechodov@free.fr>
2020-05-12 16:39:30 +0200

    Page en construction

--- atom.php
+++ atom.php
@@ -1,11 +1,13 @@
 <?php
-
 include "header.php";
-
 ?>
 
-<p> Atom </p>
+<div class="ContainerAtom">
+  <h1>Page en construction</h1>
+  <p>Merci de bien vouloir revenir plus tard</p>
+  <p>Cliquez sur l'image ci-dessous pour revenir au menu principal</p>
+  <a href="index.php"><img src="img\En_Construction.jpg" alt="En Construction"></a>
+</div>
 
 <?php
-
 include "footer.php";
