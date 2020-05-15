b63b4d0e8f01cafe37059bc8d28e945862124ed0
Philippe PERECHODOV <philippe.perechodov@free.fr>
2020-05-15 16:59:45 +0200

    Améliorations et testes divers

--- console.php
+++ console.php
@@ -6,6 +6,17 @@ include "header.php";
 
 <p> Console </p>
 
+<div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Dropdown button
+  </button>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    <a class="dropdown-item" href="#">Action</a>
+    <a class="dropdown-item" href="#">Another action</a>
+    <a class="dropdown-item" href="#">Something else here</a>
+  </div>
+</div>
+
 <?php
 
 include "footer.php";
